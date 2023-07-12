c94a106e5bd3c39b26de20f8c301f35d6a6a9f74
Den <no.den2018@yandex.ru>
2023-07-12 16:31:32 +0500

    Фиксим

---
+++ Task19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом
+// 14212 - нет, 23432 - да, 12821 - да
+
+void CheckNumber(int number)
+{
+    int theFirstFigure = number;
+    int theLastFigure = number;
+    
+}
+
+
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+CheckNumber(number);
